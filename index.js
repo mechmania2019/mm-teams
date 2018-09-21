5598f7af6d0b4befe17f9a8b4924f2bd713306e0
Pranay Prakash <pranay.gp@gmail.com>
2018-09-21 16:43:06 -0500

    auth

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const { promisify } = require("util");
 
 const mongoose = require("mongoose");
 const { send } = require("micro");
+const authenticate = require("mm-authenticate")(mongoose);
 const { Team, Script } = require("mm-schemas")(mongoose);
 
 mongoose.connect(process.env.MONGO_URL);
 mongoose.Promise = global.Promise;
 
-
-module.exports = async (req, res) => {
-  console.log('Getting teams');
+module.exports = authenticate(async (req, res) => {
+  const team = req.user;
+  if (!team.admin) {
+    return send(res, 403, "Forbidden");
+  }
+  console.log("Getting teams");
   const teams = await Team.find({}).exec();
-  let scripts = await Promise.all(
+  const scripts = await Promise.all(
     teams.map(team => Script.findById(team.latestScript).exec())
   );
-  var zipped = teams.map(function(team, i) {
+  const zipped = teams.map(function(team, i) {
     return {
-      "team": team,
-      "script": scripts[i]
-    }
+      team: team,
+      script: scripts[i]
+    };
   });
   return zipped;
-};
+});
