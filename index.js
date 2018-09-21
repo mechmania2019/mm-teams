0a89f49635851bd81df32659df35e2243d934a5d
arshiam2 <arshiam2@illinois.edu>
2018-09-21 00:54:41 -0500

    teams updated with scripts

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const { promisify } = require("util");
 
 const mongoose = require("mongoose");
 const { send } = require("micro");
-const { Team } = require("mm-schemas")(mongoose);
+const { Team, Script } = require("mm-schemas")(mongoose);
 
 mongoose.connect(process.env.MONGO_URL);
 mongoose.Promise = global.Promise;
 
+
 module.exports = async (req, res) => {
   console.log('Getting teams');
   const teams = await Team.find({}).exec();
-  return teams.map(t => t.name);
+  let scripts = await Promise.all(
+    teams.map(team => Script.findById(team.latestScript).exec())
+  );
+  var zipped = teams.map(function(team, i) {
+    return {
+      "team": team
+      "script":scripts[i]
+    }
+  });
+  return zipped;
 };
