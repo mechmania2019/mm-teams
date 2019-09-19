920f0d0eec2cbb908c08c70ef49ba860c7ca93dc
angiecheng <angie.cheng33@gmail.com>
2019-09-19 01:25:40 -0500

    fixed teams

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const { promisify } = require("util");
-
 const mongoose = require("mongoose");
 const authenticate = require("mm-authenticate")(mongoose);
 const { Team, Script } = require("mm-schemas")(mongoose);
 
 const send = (res, status, data) => (res.statusCode = status, res.end(data));
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 mongoose.Promise = global.Promise;
 
 module.exports = authenticate(async (req, res) => {
   const team = req.user;
   if (!team.admin) {
-    return send(res, 403, "Forbidden");
+    send(res, 403, "Forbidden");
+    return;
   }
   console.log("Getting teams");
-  return Team.find({}).populate('latestScript').exec();
+  send(res, 200, JSON.stringify(await Team.find({}).populate('latestScript').exec()));
 });
